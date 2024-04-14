d73acbba23e1da6f40ef9c7a0a0513000fb6aab0
Vitaly Baschenko <vitaly.baschenko@gmail.com>
2024-04-15 00:11:03 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import  {  useEffect, useState } from 'react';
 import { nanoid } from 'nanoid';
 
 import { Container } from './App.styled';
@@ -6,7 +6,37 @@ import ContactList from './ContactList/ContactList';
 import ContactForm from './ContactForm/ContactForm';
 import Filter from './Filter/Filter';
 
-export class App extends Component {
+
+export default function App() {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    
+  }, [])
+
+  return (
+     <Container>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} />
+        <h2>Contacts</h2>
+        <Filter value={filter} onChange={this.changeFilter} />
+        <ContactList
+          contacts={visibleContact}
+          onDeleteContact={this.deleteContact}
+        />
+      </Container>
+  );
+}
+
+
+
+class oldApp extends Component {
   state = {
     contacts: [
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
